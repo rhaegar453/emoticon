29b7b14ce29fbe1ec5dfb3291889c01e9640163e
shivaraj <shivarajapple@gmail.com>
2020-09-16 10:44:08 +0530

    Changes to the avatar

--- src/Components/Avatar.js
+++ src/Components/Avatar.js
@@ -5,31 +5,35 @@ import { useSpring, animated } from "react-spring";
 
 const Avatar = ({ icon }) => {
     const [expand, setExpand] = useState(false);
-    const [isAnimating, setIsAnimating] = useState(false);
-    const springProps = useSpring({
-        width: expand ? 180 : 60,
-    });
     const textProps = useSpring({
         opacity: expand ? 1 : 0,
-        width: expand ? "100%" : "0%",
+        visible: expand ? 1 : 0,
         config: {
             duration: 1000,
         },
     });
 
+    const pProps = useSpring({
+        fontSize: expand ? 14 : 0,
+        opacity: expand ? 1 : 0,
+    });
+
     return (
         <animated.div
-            style={{ ...springProps }}
             className="imageContainer"
             onMouseOver={() => setExpand(true)}
             onMouseLeave={() => setExpand(false)}
             onAnimationStart
         >
             <img src={icon} className="image"></img>
-            {expand && !isAnimating ? (
-                <animated.div style={textProps}>
-                    <p className="text1">Created By</p>
-                    <p className="text2">Shivaraj Bakale</p>
+            {expand ? (
+                <animated.div style={{ ...textProps, marginLeft: 20 }}>
+                    <animated.p className="text1" style={pProps}>
+                        Created By
+                    </animated.p>
+                    <animated.p className="text2" style={pProps}>
+                        Shivaraj Bakale
+                    </animated.p>
                 </animated.div>
             ) : null}
         </animated.div>
